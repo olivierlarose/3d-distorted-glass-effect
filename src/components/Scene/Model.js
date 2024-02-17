2bdc2c0cddccae3211cdac6e9f46116511adf63e
Olivier Larose <olivierlarose1@gmail.com>
2024-02-17 18:06:52 -0500

    adjustments

--- src/components/Scene/Model.js
+++ src/components/Scene/Model.js
@@ -4,30 +4,29 @@ import { useFrame, useThree } from '@react-three/fiber'
 import { useControls } from 'leva'
 
 export default function Model() {
-    const { nodes } = useGLTF("/medias/torrus5.glb");
+    const { nodes } = useGLTF("/medias/torrus.glb");
     const { viewport } = useThree()
     const torus = useRef(null);
-    const node = nodes.Torus002
     
     useFrame( () => {
         torus.current.rotation.x += 0.02
     })
 
     const materialProps = useControls({
-        thickness: { value: 5, min: 0, max: 20 },
+        thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
         roughness: { value: 0, min: 0, max: 1, step: 0.1 },
         transmission: {value: 1, min: 0, max: 1, step: 0.1},
         ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
         chromaticAberration: { value: 0.02, min: 0, max: 1},
-        backside: { value: true}
+        backside: { value: true},
     })
     
     return (
-        <group scale={viewport.width / 40} >
-            <Text font={'/fonts/PPNeueMontreal-Bold.otf'} position={[0, 0, -5]} fontSize={5} color="white" anchorX="center" anchorY="middle">
+        <group scale={viewport.width / 3} >
+            <Text font={'/fonts/PPNeueMontreal-Bold.otf'} position={[0, 0, -5]} fontSize={0.35} color="white" anchorX="center" anchorY="middle">
                 hello world!
             </Text>
-            <mesh ref={torus} {...node}>
+            <mesh ref={torus} {...nodes.Torus002}>
                 <MeshTransmissionMaterial {...materialProps}/>
             </mesh>
         </group>
